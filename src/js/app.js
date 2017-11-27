d2acd3b614639b414f77ae4d0292d57c889d0ead
杨攀 <yangpan@baixing.com>
2017-11-27 18:17:18 +0800

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+
+var width = window.innerWidth
+var SW = new SiriWave({
+    width: 500,
+    height: 100,
+    speed: 0.03,
+    amplitude: 1,
+    container: document.getElementById('siric'),
+    autostart: true,
+    color : '#000',
+    style: 'ios9',
+});
